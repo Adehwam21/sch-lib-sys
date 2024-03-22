2c9463431877bee55dacf3022a0eb13794c0b849
Adehwam21 <drewaaron96@gmail.com>
2024-03-22 11:54:46 +0000

    Add DTO for type checks

---
+++ src/dto/authDto.ts
@@ -0,0 +1,14 @@
+export interface Register {
+    firstName: string;
+    lastName: string;
+    username: string;
+    age: number;
+    password: string;
+    email: string;
+    phone: string;
+}
+
+export interface Login {
+    username: string;
+    password: string;
+}
---
+++ src/dto/userRelatedDto.ts
@@ -0,0 +1,6 @@
+export interface UpdateUser {
+    firstName: string;
+    lastName: string;
+    age: number;
+    phone: string;
+}
