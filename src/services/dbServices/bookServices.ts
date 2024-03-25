5f5ed8c4e852bfd3bfcf9896f411ccaafd4588ac
Adehwam21 <drewaaron96@gmail.com>
2024-03-25 16:34:53 +0000

    Update databsase services for books

---
+++ src/services/dbServices/bookServices.ts
@@ -0,0 +1,160 @@
+import { prisma } from "../../db/prismas";
+
+//  CREATE BOOK
+export const createBookService = async (title: string, authorId: string, authorFirstName: string, authorLastName: string) => {
+    try {
+        // Verify if author data exists in the database.
+        const author = await prisma.author.findUnique({
+            where: { id: authorId }
+        });
+
+        if (!author) {
+            return {code: 401, data: "Author does not exist. Consider creating an author profile"};
+        }
+
+        // Verify if book exists in the database.
+        const existingBook = await prisma.book.findUnique({
+            where: { title: title }
+        });
+
+        if (existingBook) {
+            return {code: 401, data: "Book already exists"};
+        }
+
+        // Create the book
+        const createBook = await prisma.book.create({
+            data: {
+                title: title,
+                authorFirstName: authorFirstName,
+                authorLastName: authorLastName,
+                authorId: authorId
+            }
+        });
+
+        return {code: 201, data: "Book created"};
+    } catch (error) {
+        throw new Error("Error adding book: " + error);
+    }
+};
+
+// GET ALL BOOKS
+export const getAllBooksService = async () => {
+    try {
+        const books = await prisma.book.findMany();
+        return {code: 200, data: books};
+    } catch (error) {
+        throw new Error("Error fetching all books: " + error);
+    }
+};
+
+// GET BOOK BY ID
+export const getBookByIdService = async (id: string) => {
+    try {
+        const book = await prisma.book.findUnique({
+            where: { id: id }
+        });
+        return {code: 200, data: book};
+    } catch (error) {
+        throw new Error("Error fetching book by id: " + error);
+    }
+};
+
+// GET BOOK BY TITLE
+export const getBookByTitleService = async (title: string) => {
+    try {
+        const book = await prisma.book.findUnique({
+            where: { title: title }
+        });
+        return {code: 200, data: book};
+    } catch (error) {
+        throw new Error("Error fetching book by title: " + error);
+    }
+};
+
+//Add update book copy later
+
+// DELETE BOOK BY ID
+export const deleteBookByIdService = async (id: string) => {
+    try {
+        const book = await prisma.book.delete({
+            where: { id: id }
+        });
+        return {code: 200, data: book};
+    } catch (error) {
+        throw new Error("Error deleting book: " + error);
+    }
+};
+
+
+/* ================================== BOOK COPIES ==============================================*/
+export const createBookCopyService = async (bookId: string, copyCode: number, ISBN: string, pages: number) => {
+    try {
+        // Verify if book exists in the database
+        const book = await prisma.book.findUnique({
+            where: { id: bookId }
+        });
+
+        if (!book) {
+            throw new Error("This copy does not refer to any book");
+        }
+
+        // Verify if a copy of the book already exists
+        const existingBookCopy = await prisma.bookCopy.findUnique({
+            where: { copyCode: copyCode }
+        });
+
+        if (existingBookCopy) {
+            throw new Error("This copy already exists");
+        }
+
+        // Create the book copy
+        const createBookCopy = await prisma.bookCopy.create({
+            data: {
+                copyCode: copyCode,
+                bookId: bookId,
+                ISBN: ISBN,
+                pages: pages
+            }
+        });
+
+        return {code: 201, data: 'Copy created successfully'};
+    } catch (error) {
+        throw new Error("Error adding book copy: " + error);
+    }
+};
+
+// GET ALL BOOK COPIES
+export const getAllBookCopiesService = async () => {
+    try {
+        const bookCopies = await prisma.bookCopy.findMany();
+        return {code: 200, data: bookCopies}
+    } catch (error) {
+        throw new Error("Error fetching all book copies: " + error);
+    }
+};
+
+// GET COPY BY COPY CODE
+export const getBookCopyByCodeService = async (copyCode: string) => {
+    try {
+        const bookCopy = await prisma.bookCopy.findUnique({
+            where: { copyCode: copyCode }
+        });
+        return {code: 200, data: bookCopy}
+    } catch (error) {
+        throw new Error("Error fetching book copy by code: " + error);
+    }
+};
+
+//Add update book copy later
+
+// DELETE BOOK COPY
+export const deleteBookCopyByCodeService = async (copyCode: string) => {
+    try {
+        const deletedBookCopy = await prisma.bookCopy.delete({
+            where: { copyCode: copyCode }
+        });
+        return {code: 200, data: deletedBookCopy}
+    } catch (error) {
+        throw new Error("Error deleting book copy: " + error);
+    }
+};
