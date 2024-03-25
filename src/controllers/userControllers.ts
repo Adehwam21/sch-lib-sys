1a7634b047ab3a8bba3127fdf5daa70f655c48a5
Adehwam21 <drewaaron96@gmail.com>
2024-03-25 15:09:37 +0000

    Update controller logic

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -1,12 +1,13 @@
 import { UpdateUser } from 'dto/userRelatedDto';
 import {prisma} from '../db/prismas';
 import {Request, Response} from 'express'
+import { getAllUsersService, getUserByIdService, getUserByUsernameService, getUserByMailService, getUserByRoleService, updateUserService } from '../services/dbServices/userServices';
 
 /*=========================== Get all Users ======================================*/
 export const getAllUsers = async (req: Request, res: Response) => {
     try {
-        const users = await prisma.user.findMany();
-        return res.status(200).json({ users });
+        const { code, data } = await getAllUsersService();
+        return res.status(code).json(data);
     } catch (error) {
         console.error("Error fetching all users:", error);
         return res.status(500).json({ message: "Internal server error" });
@@ -17,16 +18,8 @@ export const getAllUsers = async (req: Request, res: Response) => {
 export const getUserById = async (req: Request, res: Response) => {
     const { userId } = req.params;
     try {
-        const user = await prisma.user.findUnique({
-            where: { id: String(userId) },
-        });
-
-        if (!user) {
-            console.log("User not found");
-            return res.status(404).json({ message: "User not found" });
-        }
-
-        return res.status(200).json({ user });
+        const { code, data } = await getUserByIdService(userId);
+        return res.status(code).json(data);
     } catch (error) {
         console.error("Error fetching user details:", error);
         return res.status(500).json({ message: "Internal server error" });
@@ -37,16 +30,8 @@ export const getUserById = async (req: Request, res: Response) => {
 export const getUserByUsername = async (req: Request, res: Response) => {
     const { username } = req.params;
     try {
-        const user = await prisma.user.findUnique({
-            where: { username: String(username) },
-        });
-
-        if (!user) {
-            console.log("User not found");
-            return res.status(404).json({ message: "User not found" });
-        }
-
-        return res.status(200).json({ user });
+        const { code, data } = await getUserByUsernameService(username);
+        return res.status(code).json(data);
     } catch (error) {
         console.error("Error fetching user details:", error);
         return res.status(500).json({ message: "Internal server error" });
@@ -55,18 +40,10 @@ export const getUserByUsername = async (req: Request, res: Response) => {
 
 /*=========================== Find user by email ======================================*/
 export const getUserByMail = async (req: Request, res: Response) => {
-    const { userId } = req.params;
+    const { email } = req.params;
     try {
-        const user = await prisma.user.findUnique({
-            where: { id: String(userId) },
-        });
-
-        if (!user) {
-            console.log("User not found");
-            return res.status(404).json({ message: "User not found" });
-        }
-
-        return res.status(200).json({ user });
+        const { code, data } = await getUserByMailService(email);
+        return res.status(code).json(data);
     } catch (error) {
         console.error("Error fetching user details:", error);
         return res.status(500).json({ message: "Internal server error" });
@@ -78,16 +55,8 @@ export const getUserByRole = async (req: Request, res: Response) => {
     const { role } = req.query;
     
     try {
-        const user = await prisma.user.findMany({
-            where: { role: role},
-        });
-
-        if (!user) {
-            console.log("User not found");
-            return res.status(404).json({ message: "User not found" });
-        }
-
-        return res.status(200).json({ user });
+        const { code, data } = await getUserByRoleService(role as string);
+        return res.status(code).json(data);
     } catch (error) {
         console.error("Error fetching user details:", error);
         return res.status(500).json({ message: "Internal server error" });
@@ -100,33 +69,11 @@ export const updateUser = async (req: Request, res: Response) => {
     const { firstName, lastName, age, phone }: UpdateUser = req.body;
 
     try {
-        // Fetch author information from the database
-        const user = await prisma.user.findUnique({
-            where: { id: id }
-        });
-
-        if (!user) {
-            console.log("User does not exist");
-            return res.status(404).json({ message: "User not found" });
-        }
-
-        // Update author details
-        await prisma.user.update({
-            where: { id: String(id) },
-            data: {
-                firstName: firstName || user?.firstName,
-                lastName: lastName || user?.lastName,
-                age: age || user?.age,
-                phone: phone || user?.email
-            }
-        });
-
-        console.log("Update successful");
-        return res.status(200).json({
-            message: "User details updated successfully"
-        });
+        const { code, data } = await updateUserService(id, firstName, lastName, age, phone);
+        return res.status(code).json({ data });
     } catch (error) {
         console.log("Error updating User", error);
         return res.status(500).json({ message: "Internal server error" });
     }
 };
+
