0b531cd02519f55f0ffafac936330cd819cbc8ea
Adehwam21 <drewaaron96@gmail.com>
2024-03-27 15:29:54 +0000

    Fix: Update user endpoint route paths

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -11,12 +11,12 @@ import {
     deleteUser
 } from "../controllers/userControllers";
 
-router.get('/users', getAllUsers);                  // Route for fetching all users.
-router.get('/user/:id', getUserById);               // Route for fetching users by ID.      
-router.get('/user', getUserByMail);                 // Route for fetching user by ID.
-router.get('/users/role', getUserByRole);                // Route for fetching user by role.
-router.get('/user/:username', getUserByUsername)    // Route for fetching user by username.
-router.put('/update-user/:id', updateUser);   // Route for updating user data.
-router.delete('/delete-user/:id', deleteUser);      // Route for deleting user data.
+router.get('/users', getAllUsers);                              // Route for fetching all users.
+router.get('/users/id/:id', getUserById);                       // Route for fetching users by ID.      
+router.get('/users/email', getUserByMail);                      // Route for fetching user by ID.
+router.get('/users/role', getUserByRole);                       // Route for fetching user by role.
+router.get('/users/username/:username', getUserByUsername)     // Route for fetching user by username.
+router.put('/users/update/:id', updateUser);                   // Route for updating user data.
+router.delete('/users/delete/:id', deleteUser);                // Route for deleting user data.
 
 module.exports = router;
